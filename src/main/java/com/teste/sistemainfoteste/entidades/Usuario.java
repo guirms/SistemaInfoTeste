a76088ea6134a559f49200cb7f9c3542734f93b6
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-02-21 02:47:46 -0300

    Adicionando lombok na entidade

--- src/main/java/com/teste/sistemainfoteste/entidades/Usuario.java
+++ src/main/java/com/teste/sistemainfoteste/entidades/Usuario.java
@@ -1,20 +1,32 @@
 package com.teste.sistemainfoteste.entidades;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Setter(value = AccessLevel.NONE)
 	private Long id;
 
 	private String usuarioLogin;
@@ -31,81 +43,10 @@ public class Usuario implements Serializable {
 
 	private String codigo;
 
-	public Usuario() {
-	}
-
 	public Usuario(Long id, String usuarioLogin, String senhaLogin) {
 		this.id = id;
 		this.usuarioLogin = usuarioLogin;
 		this.senhaLogin = senhaLogin;
-	}
-
-	public Usuario(Long id, String usuarioLogin, String senhaLogin, String nome, String cpf, String endereco,
-			String telefone, String codigo) {
-		this.id = id;
-		this.usuarioLogin = usuarioLogin;
-		this.senhaLogin = senhaLogin;
-		this.nome = nome;
-		this.cpf = cpf;
-		this.endereco = endereco;
-		this.telefone = telefone;
-		this.codigo = codigo;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsuarioLogin() {
-		return usuarioLogin;
-	}
-
-	public void setUsuarioLogin(String usuarioLogin) {
-		this.usuarioLogin = usuarioLogin;
-	}
-
-	public String getSenhaLogin() {
-		return senhaLogin;
-	}
-
-	public void setSenhaLogin(String senhaLogin) {
-		this.senhaLogin = senhaLogin;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
 	}
 
 	public String gerarCodigo() {
@@ -130,31 +71,6 @@ public class Usuario implements Serializable {
 		}
 
 		return codigo;
-	}
-
-	public String getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(id, other.id);
 	}
 
 }
